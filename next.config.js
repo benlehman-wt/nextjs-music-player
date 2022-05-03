7345ae08aa08a0bf5f6c6681f08ede7b81990b5f
benlehman <benlehman@WTMBP-Ben-Lehman.local>
2022-05-03 16:00:55 -0400

    added prisma schema and set up auth routes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  serverRuntimeConfig: {
+    JWT_SECRET:
+      '1883ffd3-b091-4aa5-b5b2-13fc6b38a6bf-788c10ee-e603-4424-9686-dfd2c35f2726-1c7fc108-3b3a-471c-b206-ba5795431159',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
